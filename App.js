48f9ffa6fd44b4091c508a473359c99e963e5151
Philip Kabranov <pkabranov@Philips-MacBook-Pro.local>
2021-11-30 20:17:30 -0800

    item registry

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
 import { NativeBaseProvider, Box } from "native-base";
 import SignInScreen from "./screens/AuthStack/SignInScreen";
 import SignUpScreen from "./screens/AuthStack/SignUpScreen";
-import { createStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import { AuthStackScreen } from "./screens/AuthStack/AuthStackScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import FeedScreen from "./screens/AuthStack/FeedScreen/FeedScreen.main";
+import DetailScreen from "./screens/AuthStack/DetailScreen/DetailScreen.main";
+import firebase from 'firebase'
 
-// const Stack = createStackNavigator();
+AppRegistry.registerComponent('main', () => App);
+
+const firebaseConfig = require("./keys.json");
+if (firebase.apps.length == 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-        <AuthStackScreen />
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Feed"
+            component={FeedScreen}
+          />
+          <Stack.Screen
+            name="Detail"
+            component={DetailScreen}
+          />
+        </Stack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
   );
